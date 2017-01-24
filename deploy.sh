85f74eba0c6f9b1d5ee25bdb38a7cadf1e27c539
Mehul Kumar <Gouenji@users.noreply.github.com>
2017-01-25 01:27:24 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 athena.py
+
+# Commit changes
+echo "commiting.."
+git add -A
+git commit -m 'Update and Build'
+
+# Create the subtree, push changes to gh-pages
+# git subtree split --prefix output -b gh-pages
+
+echo "deploying.."
+git push origin master
+# git push -f origin gh-pages:gh-pages
+
+# echo "cleaning..."
+# git branch -D gh-pages
